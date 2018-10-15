097c962b719bbdbcdb02798cf152752da730ea8a
Angelica Winkler <carowink3@gmail.com>
2018-10-15 17:49:53 -0400

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,30 @@
-class CrewMember {}
+let crewMemberId = 0;
+let store = {crew: [], spaceShips: []}
+
+class CrewMember {
+  constructor(position){
+    this.position = position;
+    this.currentShip = "Looking for a Rig";
+  }
+  engageWarpDrive(){
+    if (this.currentShip === "Looking for a Rig"){
+      return "had no effect";
+    } else if (this.position === "Pilot"){
+      return this.currentShip.warpDrive = "engaged"
+    }
+  }
+  setsInvisibility(){
+    if (this.currentShip === "Looking for a Rig"){
+      return "had no effect";
+    } else if (this.position === "Defender"){
+      return this.currentShip.cloaked = true;
+    }
+  }
+  chargePhasers(){
+    if (this.currentShip === "Looking for a Rig"){
+      return "had no effect";
+    } else if (this.position === "gunner"){
+      return this.currentShip.phasersCharge = "charged"
+    }
+  }
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,22 @@
-class Spaceship {}
+let spaceshipId = 0;
+
+class Spaceship {
+  constructor(name, crew, phasers, shields){
+    this.name = name;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.crew = crew;
+    this.cloaked = false;
+    this.warpDrive = "disengaged";
+    this.phasersCharge = "uncharged";
+    if (this.crew.length){
+      this.docked = false;
+    } else {
+      this.docked = true;
+    }
+    this.association();
+  }
+  association(){
+    this.crew.forEach(member => member.currentShip = this)
+  }
+}
