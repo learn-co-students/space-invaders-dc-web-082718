7466f78c8830d7759e6d4d3b5b8d25a5e0e6fbcf
Srikant Kumar Kalaputapu <skalaputapu@gmail.com>
2018-10-26 18:00:59 -0400

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,39 @@
-class CrewMember {}
+class CrewMember {
+
+    constructor(position, current = "Looking for a Rig"){
+        this.position = position;
+        this.currentShip = current;
+    }
+
+    engageWarpDrive(){
+        if(this.currentShip === "Looking for a Rig" || this.position != "Pilot"){
+            return "had no effect";
+        }
+        else{
+            this.currentShip.warpDrive ="engaged";
+        }
+
+
+    }
+
+
+    setsInvisibility(){
+        if(this.currentShip === "Looking for a Rig" || this.position != "Defender"){
+            return "had no effect";
+        }
+        else{
+            this.currentShip.cloaked = true;
+        }
+
+    }
+
+    chargePhasers(){
+        if(this.currentShip === "Looking for a Rig" || this.position != "Gunner"){
+            return "had no effect";
+        }
+        else{
+            this.currentShip.phasersCharge = "charged";
+        }
+    }
+
+}
